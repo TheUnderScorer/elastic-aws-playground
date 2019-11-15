1a564315eb02415f4f98e776430bd1ee37661d1c
Przemysław Żydek <przemyslaw.zydek@tsh.io>
2019-11-15 10:59:30 +0100

    Update migration

--- packages/postgres/src/migrations/1573811300060-main.ts
+++ packages/postgres/src/migrations/1573811300060-main.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class main1573804842291 implements MigrationInterface {
-    name = 'main1573804842291'
+export class main1573811300060 implements MigrationInterface {
+    name = 'main1573811300060'
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`CREATE TABLE "record_model" ("id" SERIAL NOT NULL, "data" jsonb NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d59450740568d2c9187de002b45" PRIMARY KEY ("id"))`, undefined);

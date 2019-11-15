7a3fbc556a3364bee8cb260eed96c94f4160a442
Przemysław Żydek <przemyslaw.zydek@tsh.io>
2019-11-15 13:24:04 +0100

    Migrate to yarn

--- packages/aws/src/sqs/client.ts
+++ packages/aws/src/sqs/client.ts
@@ -1,10 +1,10 @@
-import {SQS} from "aws-sdk";
+import { SQS } from 'aws-sdk';
 
 export const endpoint = 'http://localhost:4576/';
 export const queueUrl = `${endpoint}/playground`;
 
 export const client = new SQS({
-    endpoint,
-    region: 'us-east-1',
-    apiVersion: 'latest',
+  endpoint,
+  region: 'us-east-1',
+  apiVersion: 'latest',
 });
